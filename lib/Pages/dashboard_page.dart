1deb86f306eaa7585c59d53e511c94105d6faa35
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2021-12-18 22:26:32 +0600

    DATE FORMATTING SUCCESSFUL!!!!

--- lib/Pages/dashboard_page.dart
+++ lib/Pages/dashboard_page.dart
@@ -4,11 +4,14 @@ import 'package:gosol_tracker_app/Controller/gosol_controller.dart';
 import 'package:gosol_tracker_app/Database/database_helper.dart';
 import 'package:gosol_tracker_app/Model/gosol_model.dart';
 import 'package:gosol_tracker_app/Pages/enter_new_gosol.dart';
+import 'package:intl/intl.dart';
 
 class DashboardPage extends StatelessWidget {
   DashboardPage({Key? key}) : super(key: key);
 
   GosolController controller = Get.put(GosolController());
+
+  DateFormat f = DateFormat("dd-MMM hh:mm a");
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +35,8 @@ class DashboardPage extends StatelessWidget {
             final gosol = controller.gosolList.value[index];
 
             return ListTile(
-              title: Text(DateTime.fromMicrosecondsSinceEpoch(gosol.datetime!)
-                  .toString()),
+              title: Text(f.format(
+                  DateTime.fromMicrosecondsSinceEpoch(gosol.datetime!))),
             );
           },
         ),
