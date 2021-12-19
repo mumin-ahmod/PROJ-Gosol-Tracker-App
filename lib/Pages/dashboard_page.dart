1a286ac75c0c0046d6cdc2653bef4f614e9ee9a9
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2021-12-19 21:06:20 +0600

    Batter design of dashboard page. timeline align left!

--- lib/Pages/dashboard_page.dart
+++ lib/Pages/dashboard_page.dart
@@ -27,76 +27,128 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black54),
         title: const Text(
-          "Shower Tracker App",
+          "Shower Tracker",
+          style: TextStyle(color: Colors.black54),
         ),
+        elevation: 0,
         actions: [
-          IconButton(
-              onPressed: () {
-                Get.to(() => EnterNewGosol());
-              },
-              icon: const Icon(Icons.playlist_add_outlined))
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: IconButton(
+                onPressed: () {
+                  Get.to(() => EnterNewGosol());
+                },
+                icon: const Icon(
+                  Icons.add,
+                  size: 30,
+                )),
+          )
         ],
       ),
+      drawer: Drawer(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(20.0),
             child: SizedBox(
               height: 250,
               width: 400,
-              child: Card(
-                color: Colors.black54,
-                child: Center(
-                  child: Obx(
-                    () => Text(
-                      "Today \nYou are UnGosoled For: \n${getUnday()} Days",
-                      style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                      textAlign: TextAlign.center,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const CircleAvatar(
+                          radius: 35,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Mumin Ahmod",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "🗺️ Mymensingh",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                ),
+                  Center(
+                    child: Obx(
+                      () => Text(
+                        "Today \nYou are UnGosoled For: \n${getUnday()} Days",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
           Flexible(
             child: Obx(
-              () => ListView(children: [
-                FixedTimeline.tileBuilder(
-                  builder: TimelineTileBuilder.connectedFromStyle(
-                    itemCount: controller.gosolList.value.length,
-                    contentsBuilder: (context, index) => Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                          "UnGosoled For: \n${DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index].datetime!).day - DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index == 0 ? 0 : index - 1].datetime!).day} Days"),
-                    ),
-                    oppositeContentsBuilder: (context, index) {
-
-                      final gosol = controller.gosolList.value[index];
-
-                      return Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Card(
-                          child: Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: Text(f.format(
-                                DateTime.fromMicrosecondsSinceEpoch(
-                                    gosol.datetime!))),
-                          ),
+              () => SizedBox(
+                width: 300,
+                child: ListView(children: [
+                  FixedTimeline.tileBuilder(
+                    builder: TimelineTileBuilder.connectedFromStyle(
+                      itemCount: controller.gosolList.value.length,
+                      contentsBuilder: (context, index) => SizedBox(
+                        height: 100,
+                        width: 400,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                              "UnGosoled For: \n${DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index].datetime!).day - DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index == 0 ? 0 : index - 1].datetime!).day} Days"),
                         ),
-                      );
-                    },
-                    connectorStyleBuilder: (context, index) {
-                      return ConnectorStyle.solidLine;
-                    },
-                    indicatorStyleBuilder: (context, index) =>
-                        IndicatorStyle.dot,
+                      ),
+                      oppositeContentsBuilder: (context, index) {
+                        final gosol = controller.gosolList.value[index];
+
+                        return Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Card(
+                            child: Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(f.format(
+                                  DateTime.fromMicrosecondsSinceEpoch(
+                                      gosol.datetime!))),
+                            ),
+                          ),
+                        );
+                      },
+                      connectorStyleBuilder: (context, index) {
+                        return ConnectorStyle.solidLine;
+                      },
+                      indicatorStyleBuilder: (context, index) =>
+                          IndicatorStyle.dot,
+                    ),
                   ),
-                ),
-              ]),
+                ]),
+              ),
             ),
           ),
         ],
