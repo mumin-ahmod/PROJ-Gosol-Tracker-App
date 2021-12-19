f2b7ebb84e70ece61a4e790dd7c7db9c399b99fc
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2021-12-19 15:06:16 +0600

    ALL CALCULATION IS WORKING! UNDAY & UNGOSOLED.

--- lib/Pages/dashboard_page.dart
+++ lib/Pages/dashboard_page.dart
@@ -12,6 +12,17 @@ class DashboardPage extends StatelessWidget {
 
   DateFormat f = DateFormat("dd-MMM \nh:mm a");
 
+  int getUnday() {
+    if (controller.gosolList.isNotEmpty) {
+      DateTime dateLast = DateTime.fromMicrosecondsSinceEpoch(controller
+          .gosolList.value[controller.gosolList.value.length - 1].datetime!);
+
+      return controller.unDay.value = DateTime.now().day - dateLast.day;
+    } else {
+      return 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +48,15 @@ class DashboardPage extends StatelessWidget {
               child: Card(
                 color: Colors.black54,
                 child: Center(
-                  child: Text(
-                    "Today \nYou are UnGosoled For: \n${controller.unDay.value} Days",
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                    textAlign: TextAlign.center,
+                  child: Obx(
+                    () => Text(
+                      "Today \nYou are UnGosoled For: \n${getUnday()} Days",
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               ),
@@ -61,16 +74,7 @@ class DashboardPage extends StatelessWidget {
                           "UnGosoled For: \n${DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index].datetime!).day - DateTime.fromMicrosecondsSinceEpoch(controller.gosolList.value[index == 0 ? 0 : index - 1].datetime!).day} Days"),
                     ),
                     oppositeContentsBuilder: (context, index) {
-                      // DateTime dateLast = DateTime.fromMicrosecondsSinceEpoch(
-                      //     controller
-                      //         .gosolList
-                      //         .value[controller.gosolList.value.length - 1]
-                      //         .datetime!);
-                      //
 
-                      //
-                      // controller.unDay.value =
-                      //     DateTime.now().day - dateLast.day;
                       final gosol = controller.gosolList.value[index];
 
                       return Padding(
@@ -85,8 +89,9 @@ class DashboardPage extends StatelessWidget {
                         ),
                       );
                     },
-                    connectorStyleBuilder: (context, index) =>
-                        ConnectorStyle.solidLine,
+                    connectorStyleBuilder: (context, index) {
+                      return ConnectorStyle.solidLine;
+                    },
                     indicatorStyleBuilder: (context, index) =>
                         IndicatorStyle.dot,
                   ),
